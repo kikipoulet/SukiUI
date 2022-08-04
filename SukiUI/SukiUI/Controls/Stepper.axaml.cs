7d4dfdc71d3d468f273848eda0661d37395a3e18
Clément Sepulchre <sepulchreclement.dic@gmail.com>
2022-08-04 11:21:29 +0000

    Suki 3.0.0

---
+++ SukiUI/SukiUI/Controls/Stepper.axaml.cs
@@ -0,0 +1,143 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System;
+using System.Collections.Generic;
+using Avalonia.Layout;
+using Avalonia.Media;
+
+namespace SukiUI.Controls
+{
+    public partial class Stepper : UserControl
+    {
+        public Stepper()
+        {
+            InitializeComponent();
+            Update();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            
+        }
+
+        public static readonly StyledProperty<int> IndexProperty =
+       AvaloniaProperty.Register<Stepper, int>(nameof(Index), defaultValue: 0);
+
+        public int Index
+        {
+            get { return GetValue(IndexProperty); }
+            set
+            {
+                if (value < 0 || value > Steps.Count -1)
+                    return;
+                SetValue(IndexProperty, value ); 
+                Update(); }
+        }
+
+        public static readonly StyledProperty<List<string>> StepsProperty =
+          AvaloniaProperty.Register<Stepper, List<string>>(nameof(Steps), defaultValue: new List<string>() { "stepper"});
+
+        public List<string> Steps
+        {
+            get { return GetValue(StepsProperty); }
+            set { SetValue(StepsProperty, value); Update(); }
+        }
+
+        public void Update()
+        {
+            Grid grid = this.FindControl<Grid>("gridStepper");
+            grid.Children.Clear();
+
+            SetColumnDefinitions(grid);
+            
+            for (var i = 0; i < Steps.Count; i++)
+            {
+                AddStep(Steps[i], i, grid);
+            }
+            
+        }
+
+        private void SetColumnDefinitions(Grid grid)
+        {
+            var columns = new ColumnDefinitions();
+            Steps.ForEach(s => columns.Add(new ColumnDefinition()));
+            grid.ColumnDefinitions = columns;
+        }
+
+        private void AddStep(string step, int index,Grid grid)
+        {
+            Brush PrimaryColor = new SolidColorBrush(Color.FromRgb(47, 84, 235));
+            Brush DisabledColor = new SolidColorBrush(Color.FromRgb(222, 222, 222));
+            
+            var griditem = new Grid(){ ColumnDefinitions = new ColumnDefinitions(){new ColumnDefinition(), new ColumnDefinition()}};
+
+            var line = new Border() { CornerRadius = new CornerRadius(3),  Margin = new Thickness(-5,0,23,0), Background = Brushes.WhiteSmoke, Height = 2, HorizontalAlignment = HorizontalAlignment.Stretch, VerticalAlignment = VerticalAlignment.Center };
+            var line1 = new Border() { CornerRadius = new CornerRadius(3),  Margin = new Thickness(23,0,-5,0), Background = Brushes.WhiteSmoke, Height = 2, HorizontalAlignment = HorizontalAlignment.Stretch, VerticalAlignment = VerticalAlignment.Center };
+
+            if (index == 0)
+                line.IsVisible = false;
+            if (index == Steps.Count -1)
+                line1.IsVisible = false;
+
+            if (index == Index)
+                line.Background = PrimaryColor;
+
+            if (index < Index)
+            {
+                line1.Background = PrimaryColor;
+                line.Background = PrimaryColor;
+            }
+            
+            Grid.SetColumn(line,0);
+            Grid.SetColumn(line1,1);
+            
+            griditem.Children.Add(line);
+            griditem.Children.Add(line1);
+
+            var gridBorder = new Grid();
+            
+            var circle = new Border()
+                { Margin = new Thickness(0,0,0,2), Height = 30, Width = 30, CornerRadius = new CornerRadius(25), HorizontalAlignment = HorizontalAlignment.Center, VerticalAlignment = VerticalAlignment.Center };
+
+            if (index == Index)
+            {
+                circle.Background = PrimaryColor;
+                
+                circle.BorderThickness = new Thickness(0);
+                circle.Child = new TextBlock() {VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center, Text = (index + 1).ToString(), Foreground = Brushes.White};
+            }
+            else if (index < Index)
+            {
+                circle.Background = Brushes.White;
+                circle.BorderThickness = new Thickness(1.5);
+                circle.BorderBrush = PrimaryColor;
+                circle.Child = new TextBlock() {VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center, Text = (index + 1).ToString(), Foreground = PrimaryColor};
+            }
+            else
+            {
+                circle.Background = Brushes.White;
+                circle.BorderThickness = new Thickness(1.5);
+                circle.BorderBrush = DisabledColor;
+                circle.Child = new TextBlock() { VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center, Text = (index + 1).ToString(), Foreground = DisabledColor};
+            }
+            
+
+            
+            
+            gridBorder.Children.Add(circle);
+            
+            gridBorder.Children.Add(new TextBlock()
+            {
+                FontWeight = index == Index ? FontWeight.Medium: FontWeight.Normal,
+                Text = step, VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center, Margin = new Thickness(0,55,0,0)
+            });
+            
+            Grid.SetColumn(griditem,index);
+            Grid.SetColumn(gridBorder,index);
+            grid.Children.Add(griditem);
+            grid.Children.Add(gridBorder);
+        }
+    }
+}
---
+++ SukiUI/SukiUI/MessageBox/MessageBox.axaml.cs
@@ -0,0 +1,72 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Material.Icons.Avalonia;
+using SukiUI.Controls;
+using System.Threading.Tasks;
+
+namespace SukiUI.MessageBox
+{
+    public partial class MessageBox : Window
+    {
+
+      
+        public MessageBox()
+        {
+            InitializeComponent();
+
+        }
+
+        public MessageBox(string Title, string Message)
+        {
+            InitializeComponent();
+            this.FindControl<TextBlock>("Title").Text = Title;  
+            this.FindControl<TextBlock>("Message").Text = Message;  
+
+        }
+
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public static void Info(Window owner, string Title, string Message, WindowStartupLocation startupLocation = WindowStartupLocation.CenterScreen)
+        {
+            var mbox = new MessageBox(Title, Message);
+            mbox.FindControl<MaterialIcon>("Icone").Kind = Material.Icons.MaterialIconKind.InformationCircle;
+            mbox.FindControl<MaterialIcon>("Icone").Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom("#2f54eb"));
+            mbox.WindowStartupLocation = startupLocation;
+            
+            mbox.ShowDialog(owner);
+        }
+
+        public static void Success(Window owner, string Title, string Message, WindowStartupLocation startupLocation = WindowStartupLocation.CenterScreen)
+        {
+            var mbox = new MessageBox(Title, Message);
+            mbox.FindControl<MaterialIcon>("Icone").Kind = Material.Icons.MaterialIconKind.CheckboxMarkedCircleOutline;
+            mbox.FindControl<MaterialIcon>("Icone").Foreground = Brushes.DarkGreen;
+            mbox.WindowStartupLocation = startupLocation;
+            mbox.ShowDialog(owner);
+        }
+
+        public static void Error(Window owner, string Title, string Message, WindowStartupLocation startupLocation = WindowStartupLocation.CenterScreen)
+        {
+            var mbox = new MessageBox(Title, Message);
+            mbox.FindControl<MaterialIcon>("Icone").Kind = Material.Icons.MaterialIconKind.CloseCircleOutline;
+            mbox.FindControl<MaterialIcon>("Icone").Foreground = Brushes.DarkRed;
+            mbox.WindowStartupLocation = startupLocation;
+            mbox.ShowDialog(owner);
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+       
+
+    }
+}
