415b0b41dcb7c4d4e7870a5cbfd1704eb618a659
Clement Sepulchre <sepulchreclement.dic@gmail.com>
2022-10-31 16:52:01 +0100

    Sliver Appbar + Mobile Gifs

---
+++ SukiUI/Controls/ClassicMobilePage.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
+
+namespace SukiUI.Controls
+{
+    public partial class ClassicMobilePage : UserControl
+    {
+        public ClassicMobilePage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public static readonly StyledProperty<string> HeaderProperty =
+            AvaloniaProperty.Register<CircleProgressBar, string>(nameof(Header), defaultValue: "Header");
+
+        public string Header
+        {
+            get { return GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value ); }
+        }
+    }
+}
