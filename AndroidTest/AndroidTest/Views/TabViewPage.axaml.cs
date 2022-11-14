269ac4e52ea47be16fdc97ddad0b0d2c81fd16d5
Clement Sepulchre <sepulchreclement.dic@gmail.com>
2022-11-14 12:37:14 +0100

    Mobile TabView Control

---
+++ AndroidTest/AndroidTest/Views/TabViewPage.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AndroidTest.Views;
+
+public partial class TabViewPage : UserControl
+{
+    public TabViewPage()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
