176265c90e2a57ec532fb08a475d30ecc5741a44
Nick <Nick.K.Ireland@gmail.com>
2023-01-08 09:32:01 +1000

    Update PercentProgressBar

--- SukiUI/Controls/PercentProgressBar.axaml.cs
+++ SukiUI/Controls/PercentProgressBar.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Material.Icons.Avalonia;
@@ -11,6 +12,10 @@ public partial class PercentProgressBar : UserControl
     public PercentProgressBar()
     {
         InitializeComponent();
+        // Initialize controls in the constructor
+        _progressBar = this.FindControl<ProgressBar>("barPercent");
+        _textBlock = this.FindControl<TextBlock>("textPercent");
+        _icon = this.FindControl<MaterialIcon>("iconPercent");
     }
 
     private void InitializeComponent()
@@ -18,30 +23,54 @@ public partial class PercentProgressBar : UserControl
         AvaloniaXamlLoader.Load(this);
     }
     
-    public static readonly StyledProperty<int> ValueProperty = AvaloniaProperty.Register<Stepper, int>(nameof(Value), defaultValue: 50);
-
+    // Declare private fields for controls
+    private ProgressBar _progressBar;
+    private TextBlock _textBlock;
+    private MaterialIcon _icon;
+    
+    private int _value;
+    
+    /// <summary>
+    /// Gets the current value.
+    /// </summary>
     public int Value
     {
-        get { return GetValue(ValueProperty); }
+        get => GetValue(ValueProperty);
         set
         {
+            // Check if value is valid
             if (value is < 0 or > 100)
                 return;
-            
-            SetValue(ValueProperty, value );
 
-            this.FindControl<ProgressBar>("barPercent").Value = value;
-            
+            // Set the value and raise the PropertyChanged event
+            SetAndRaise(ValueProperty, ref _value, value);
+
+            // Update the Value of the ProgressBar control
+            _progressBar.Value = value;
+
+            // Update the Text and Opacity of the TextBlock and MaterialIcon controls
+            // based on the value of the Value property
             if (value != 100)
             {
-                this.FindControl<TextBlock>("textPercent").Text = value.ToString() + " %";
+                _textBlock.Text = $"{value} %";
             }
             else
             {
-                this.FindControl<TextBlock>("textPercent").Opacity = 0;
-                this.FindControl<MaterialIcon>("iconPercent").Opacity = 1;
-                this.FindControl<ProgressBar>("barPercent").Foreground = Brushes.ForestGreen;
+                _textBlock.Opacity = 0;
+                _icon.Opacity = 1;
+                _progressBar.Foreground = Brushes.ForestGreen;
             }
         }
     }
+    
+    /// <summary>
+    /// Defines the <see cref="Value"/> property.
+    /// </summary>
+    public static readonly DirectProperty<PercentProgressBar, int> ValueProperty =
+        AvaloniaProperty.RegisterDirect<PercentProgressBar, int>(
+            nameof(Value),
+            o => o.Value,
+            (o, v) => o.Value = v,
+            defaultBindingMode: BindingMode.TwoWay,
+            enableDataValidation: true);
 }
