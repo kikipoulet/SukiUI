e6937ea8fc10ed0ccd10443916766ed0f336b0ff
Aaron <aaron.j.king.95@gmail.com>
2023-12-25 11:09:11 +0000

    Finished reworking title bar, fixed oversized Window.
    The Window becomes oversized by 8px in Windows when maximised because Windows accounts for the system border by oversizing maximised windows by 8px.

--- SukiUI/Controls/DesktopPage.axaml.cs
+++ SukiUI/Controls/DesktopPage.axaml.cs
@@ -1,39 +1,47 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Interactivity;
 using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.VisualTree;
-using Material.Icons;
-using Material.Icons.Avalonia;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Threading;
-using Avalonia.Controls.Shapes;
-using Avalonia.Threading;
+using System.Runtime.InteropServices;
+using Avalonia.Controls.Presenters;
+using Avalonia.Styling;
 
 namespace SukiUI.Controls
 {
-  
     public partial class DesktopPage : UserControl
     {
         public DesktopPage()
         {
             InitializeComponent();
-         //   DataContext = ViewModel;
+            // Apply a style only on Windows to counteract over-sized windows when removing the system border.
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                var maxStyle = new Style(
+                    x => x.OfType<Window>()
+                        .Class("NakedWindow")
+                        .PropertyEquals(Window.WindowStateProperty, WindowState.Maximized)
+                        .Template()
+                        .OfType<ContentPresenter>()
+                        .Name("PART_ContentPresenter"));
+                maxStyle.Setters.Add(new Setter(PaddingProperty, new Thickness(8)));
+                Application.Current!.Styles.Add(maxStyle);
+            }
         }
-
-        // private DesktopPageViewModel ViewModel = new DesktopPageViewModel();
-
+        
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
         }
 
-        public static readonly StyledProperty<HorizontalAlignment> TitleHorizontalAlignmentProperty = AvaloniaProperty.Register<DesktopPage, HorizontalAlignment>(nameof(TitleHorizontalAlignment), defaultValue: HorizontalAlignment.Left);
+        public static readonly StyledProperty<HorizontalAlignment> TitleHorizontalAlignmentProperty =
+            AvaloniaProperty.Register<DesktopPage, HorizontalAlignment>(nameof(TitleHorizontalAlignment),
+                defaultValue: HorizontalAlignment.Left);
 
         public HorizontalAlignment TitleHorizontalAlignment
         {
@@ -41,7 +49,8 @@ namespace SukiUI.Controls
             set { SetValue(TitleHorizontalAlignmentProperty, value); }
         }
 
-        public static readonly StyledProperty<double> TitleFontSizeProperty = AvaloniaProperty.Register<DesktopPage, double>(nameof(TitleFontSize), defaultValue: 13);
+        public static readonly StyledProperty<double> TitleFontSizeProperty =
+            AvaloniaProperty.Register<DesktopPage, double>(nameof(TitleFontSize), defaultValue: 13);
 
         public double TitleFontSize
         {
@@ -50,7 +59,9 @@ namespace SukiUI.Controls
         }
 
 
-        public static readonly StyledProperty<FontWeight> TitleFontWeightProperty = AvaloniaProperty.Register<DesktopPage, FontWeight>(nameof(TitleFontWeight), defaultValue: FontWeight.DemiBold);
+        public static readonly StyledProperty<FontWeight> TitleFontWeightProperty =
+            AvaloniaProperty.Register<DesktopPage, FontWeight>(nameof(TitleFontWeight),
+                defaultValue: FontWeight.DemiBold);
 
         public FontWeight TitleFontWeight
         {
@@ -58,7 +69,8 @@ namespace SukiUI.Controls
             set { SetValue(TitleFontWeightProperty, value); }
         }
 
-        public static readonly StyledProperty<IBrush> LogoColorProperty = AvaloniaProperty.Register<DesktopPage, IBrush>(nameof(LogoColor), defaultValue: Brushes.DarkSlateBlue);
+        public static readonly StyledProperty<IBrush> LogoColorProperty =
+            AvaloniaProperty.Register<DesktopPage, IBrush>(nameof(LogoColor), defaultValue: Brushes.DarkSlateBlue);
 
         public IBrush LogoColor
         {
@@ -66,51 +78,56 @@ namespace SukiUI.Controls
             set { SetValue(LogoColorProperty, value); }
         }
 
-        public static readonly StyledProperty<Control> LogoContentProperty = AvaloniaProperty.Register<DesktopPage, Control>(nameof(LogoContent), defaultValue: new Border());
+        public static readonly StyledProperty<Control> LogoContentProperty =
+            AvaloniaProperty.Register<DesktopPage, Control>(nameof(LogoContent), defaultValue: new Border());
 
         public Control LogoContent
         {
             get { return GetValue(LogoContentProperty); }
             set { SetValue(LogoContentProperty, value); }
         }
-        
-        
 
-        public static readonly StyledProperty<List<MenuItem>> MenuItemsProperty = 
-            AvaloniaProperty.Register<DesktopPage, List<MenuItem>>(nameof(MenuItems), defaultValue: new List<MenuItem>());
+
+        public static readonly StyledProperty<List<MenuItem>> MenuItemsProperty =
+            AvaloniaProperty.Register<DesktopPage, List<MenuItem>>(nameof(MenuItems),
+                defaultValue: new List<MenuItem>());
 
         public List<MenuItem> MenuItems
         {
             get { return GetValue(MenuItemsProperty); }
             set { SetValue(MenuItemsProperty, value); }
         }
-        
-        public static readonly StyledProperty<string> TitleProperty = AvaloniaProperty.Register<DesktopPage, string>(nameof(Title), defaultValue: "Avalonia UI");
+
+        public static readonly StyledProperty<string> TitleProperty =
+            AvaloniaProperty.Register<DesktopPage, string>(nameof(Title), defaultValue: "Avalonia UI");
 
         public string Title
         {
             get { return GetValue(TitleProperty); }
             set { SetValue(TitleProperty, value); }
         }
-        
-        public static readonly StyledProperty<bool> ShowBottomBorderProperty = AvaloniaProperty.Register<DesktopPage, bool>(nameof(ShowBottomBorder), defaultValue: true);
+
+        public static readonly StyledProperty<bool> ShowBottomBorderProperty =
+            AvaloniaProperty.Register<DesktopPage, bool>(nameof(ShowBottomBorder), defaultValue: true);
 
         public bool ShowBottomBorder
         {
             get { return GetValue(ShowBottomBorderProperty); }
             set { SetValue(ShowBottomBorderProperty, value); }
         }
-        
-        public static readonly StyledProperty<bool> MenuVisibilityProperty = AvaloniaProperty.Register<DesktopPage, bool>(nameof(MenuVisibility), defaultValue: false);
+
+        public static readonly StyledProperty<bool> MenuVisibilityProperty =
+            AvaloniaProperty.Register<DesktopPage, bool>(nameof(MenuVisibility), defaultValue: false);
 
         public bool MenuVisibility
         {
             get { return GetValue(MenuVisibilityProperty); }
             set { SetValue(MenuVisibilityProperty, value); }
         }
-        
 
-        public static readonly StyledProperty<bool> IsMinimizeButtonEnabledProperty = AvaloniaProperty.Register<DesktopPage, bool>(nameof(IsMinimizeButtonEnabled), defaultValue: true);
+
+        public static readonly StyledProperty<bool> IsMinimizeButtonEnabledProperty =
+            AvaloniaProperty.Register<DesktopPage, bool>(nameof(IsMinimizeButtonEnabled), defaultValue: true);
 
         public bool IsMinimizeButtonEnabled
         {
@@ -118,7 +135,8 @@ namespace SukiUI.Controls
             set { SetValue(IsMinimizeButtonEnabledProperty, value); }
         }
 
-        public static readonly StyledProperty<bool> IsMaximizeButtonEnabledProperty = AvaloniaProperty.Register<DesktopPage, bool>(nameof(IsMaximizeButtonEnabled), defaultValue: true);
+        public static readonly StyledProperty<bool> IsMaximizeButtonEnabledProperty =
+            AvaloniaProperty.Register<DesktopPage, bool>(nameof(IsMaximizeButtonEnabled), defaultValue: true);
 
         public bool IsMaximizeButtonEnabled
         {
@@ -141,8 +159,9 @@ namespace SukiUI.Controls
         private void MaximizeHandler(object sender, RoutedEventArgs e)
         {
             Window hostWindow = (Window)this.VisualRoot;
-            var icon = this.GetVisualDescendants().OfType<PathIcon>().FirstOrDefault(x => (x.Name ?? "").Equals("MaximizeIcon"));
-            
+            var icon = this.GetVisualDescendants().OfType<PathIcon>()
+                .FirstOrDefault(x => (x.Name ?? "").Equals("MaximizeIcon"));
+
             if (hostWindow.WindowState != WindowState.Maximized)
             {
                 hostWindow.WindowState = WindowState.Maximized;
@@ -160,7 +179,6 @@ namespace SukiUI.Controls
         /// <summary>
         /// Closes Avalonia window
         /// </summary>
-
         private void CloseHandler(object sender, RoutedEventArgs e)
         {
             Window hostWindow = (Window)this.VisualRoot;
@@ -168,15 +186,9 @@ namespace SukiUI.Controls
         }
 
 
-
         public void SetPage(Control page)
         {
-           
-                Content = page;
-            
+            Content = page;
         }
-
-      
-
     }
 }
