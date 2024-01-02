3ca2c699db5e57b00b7425b441b8f9fbc48c4661
Aaron <aaron.j.king.95@gmail.com>
2024-01-02 15:59:26 +0000

    Introduce path icon styles.
    Started adding functionality to theming page but ran into some blockers which prevent accessing these properties outside of the `SukiWindow` context in a clean way, needs a rethink...

--- SukiUI.Demo/Features/Theming/ThemingViewModel.cs
+++ SukiUI.Demo/Features/Theming/ThemingViewModel.cs
@@ -1,12 +1,44 @@
-﻿using System.Collections.ObjectModel;
-using Avalonia.Collections;
+﻿using Avalonia.Collections;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Material.Icons;
 using SukiUI.Models;
 
 namespace SukiUI.Demo.Features.Theming;
 
-public class ThemingViewModel() : DemoPageBase("Theming", MaterialIconKind.PaletteOutline, -200)
+public partial class ThemingViewModel : DemoPageBase
 {
-    public IAvaloniaReadOnlyList<SukiColorTheme> AvailablesColors { get; } =  SukiTheme.GetInstance().ColorThemes;
+    public IAvaloniaReadOnlyList<SukiColorTheme> AvailableColors { get; }
+    
+    [ObservableProperty] private bool _isDark;
+    [ObservableProperty] private SukiColorTheme _activeTheme;
+    [ObservableProperty] private bool _animationsEnabled;
+    
+    private readonly SukiTheme _theme;
+    
+    public ThemingViewModel() : base("Theming", MaterialIconKind.PaletteOutline, -200)
+    {
+        _theme = SukiTheme.GetInstance();
+        AvailableColors = _theme.ColorThemes;
+        IsDark = _theme.ActiveBaseTheme == ThemeVariant.Dark;
+        _activeTheme = _theme.ActiveColorTheme;
+        _theme.OnBaseThemeChanged += variant =>
+        { 
+            IsDark = variant == ThemeVariant.Dark;
+        };
+        _theme.OnColorThemeChanged += theme =>
+        {
+            _activeTheme = theme;
+        };
+    }
 
+    partial void OnIsDarkChanged(bool value)
+    {
+        _theme.ChangeBaseTheme(value ? ThemeVariant.Dark : ThemeVariant.Light);
+    }
+
+    partial void OnActiveThemeChanged(SukiColorTheme value)
+    {
+        _theme.ChangeColorTheme(value);
+    }
 }
