669f765e7b6d30f9d6e43d4cb63b53929663de74
Aaron <aaron.j.king.95@gmail.com>
2024-01-02 13:42:08 +0000

    Created a simple logo.

--- SukiUI.Demo/Features/ControlsLibrary/ButtonsLibraryViewModel.cs
+++ SukiUI.Demo/Features/ControlsLibrary/ButtonsLibraryViewModel.cs
@@ -1,8 +1,22 @@
+using System.Threading;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Material.Icons;
 
 namespace SukiUI.Demo.Features.ControlsLibrary;
 
-public class ButtonsLibraryViewModel() : DemoPageBase("Buttons", MaterialIconKind.Button)
+public partial class ButtonsLibraryViewModel() : DemoPageBase("Buttons", MaterialIconKind.Button)
 {
-    
+    [ObservableProperty] private bool _isBusy;
+
+    public void ButtonClicked()
+    {
+        if (IsBusy) return;
+        Task.Run(async () =>
+        {
+            IsBusy = true;
+            await Task.Delay(3000);
+            IsBusy = false;
+        });
+    }
 }
