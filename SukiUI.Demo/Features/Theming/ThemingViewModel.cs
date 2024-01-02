f7b17451391dd7d514aacb606a6ca96c44fac842
Clement Sepulchre <sepulchreclement.dic@gmail.com>
2024-01-02 17:51:20 +0100

    Layouts and ThemingView

--- SukiUI.Demo/Features/Theming/ThemingViewModel.cs
+++ SukiUI.Demo/Features/Theming/ThemingViewModel.cs
@@ -1,12 +1,38 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using Avalonia;
 using Avalonia.Collections;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Material.Icons;
+using SukiUI.Controls;
 using SukiUI.Models;
 
 namespace SukiUI.Demo.Features.Theming;
 
-public class ThemingViewModel() : DemoPageBase("Theming", MaterialIconKind.PaletteOutline, -200)
+public partial class ThemingViewModel() : DemoPageBase("Theming", MaterialIconKind.PaletteOutline, -200)
 {
-    public IAvaloniaReadOnlyList<SukiColorTheme> AvailablesColors { get; } =  SukiTheme.GetInstance().ColorThemes;
+    public IAvaloniaReadOnlyList<SukiColorTheme> AvailablesColors { get; } = SukiTheme.GetInstance().ColorThemes;
+
+    public void SwitchToLightTheme() => SukiTheme.GetInstance().ChangeBaseTheme(ThemeVariant.Light);
+    public void SwitchToDarkTheme() => SukiTheme.GetInstance().ChangeBaseTheme(ThemeVariant.Dark);
+   
+    public bool IsLightTheme
+    {
+        get { return SukiTheme.GetInstance().ActiveBaseTheme == ThemeVariant.Light; }
+    }
+
+
+    public void SwitchToColorTheme(SukiColorTheme colorTheme) => SukiTheme.GetInstance().ChangeColorTheme(colorTheme);
+    
+    [ObservableProperty] private bool _isBackgroundAnimated ;
+
+    public void ChangeAnimated()
+    {
+        ((SukiWindow)((IClassicDesktopStyleApplicationLifetime)Application.Current.ApplicationLifetime).MainWindow)
+            .BackgroundAnimationEnabled = _isBackgroundAnimated;
+    }
+
 
 }
