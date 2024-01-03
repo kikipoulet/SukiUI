ffa9314acd6746204ac37cb2125571bd8c6ef9d1
Aaron <aaron.j.king.95@gmail.com>
2024-01-03 16:13:23 +0000

    (Mostly) fix the annoying text rendering bug in the demo app.
    Mostly fixed it in a sane way but one of the instances required I make a little bit of a mess.

--- SukiUI.Demo/Features/Splash/SplashView.axaml.cs
+++ SukiUI.Demo/Features/Splash/SplashView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Threading;
 
 namespace SukiUI.Demo.Features.Splash;
 
@@ -6,6 +7,14 @@ public partial class SplashView : UserControl
 {
     public SplashView()
     {
+        SukiTheme.GetInstance().OnBaseThemeChanged += _ =>
+        {
+            Dispatcher.UIThread.Post(() =>
+            {
+                TextBlockWithInline.IsVisible = false;
+                TextBlockWithInline.IsVisible = true;
+            });
+        };
         InitializeComponent();
     }
 }
