fc1ddb8aadead890e815ac2a043ab122ae96105d
Peter Vietense <peter.vietense@hotmail.de>
2024-01-03 12:36:34 +0100

    Added Extensions for throwing exceptions instead of returning null

---
+++ SukiUI/Extensions/ControlExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using System;
+
+namespace SukiUI.Extensions
+{
+    /// <summary>
+    /// Adds common functionality to <see cref="Control"/>.
+    /// </summary>
+    public static class ControlExtensions
+    {
+        /// <summary>
+        /// Finds the named control in the scope of the specified control.
+        /// </summary>
+        /// <typeparam name="T">The type of the control to find.</typeparam>
+        /// <param name="control">The control to look in.</param>
+        /// <param name="name">The name of the control to find.</param>
+        /// <returns>The control or throws if not found.</returns>
+        /// <exception cref="ArgumentNullException" />
+        public static T FindRequiredControl<T>(this Control? control, string name) where T : Control
+        {
+            control = control ?? throw new ArgumentNullException(nameof(control));
+
+            return control.FindControl<T>(name) ?? throw new ArgumentNullException("Could not find required control with the specified name");
+        }
+    }
+}
