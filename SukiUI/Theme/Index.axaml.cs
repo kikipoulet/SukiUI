22e1f099d914ddf283c746fc20ead72d678ecdb2
Aaron <aaron.j.king.95@gmail.com>
2024-01-07 12:22:56 +0000

    Standardize 7, 2 and 1% too because why not.

--- SukiUI/Theme/Index.axaml.cs
+++ SukiUI/Theme/Index.axaml.cs
@@ -192,18 +192,10 @@ public partial class SukiTheme : Styles
 
     private void SetColorTheme(SukiColorTheme colorTheme)
     {
-        
         SetColorWithOpacities("SukiPrimaryColor", colorTheme.Primary);
         SetColorWithOpacities("SukiAccentColor", colorTheme.Accent);
-
-        SetResource("SukiPrimaryColor7", colorTheme.Primary.WithAlpha(0.07));
-        SetResource("SukiPrimaryColor2", colorTheme.Primary.WithAlpha(0.02));
-        SetResource("SukiAccentColor1", colorTheme.Accent.WithAlpha(0.01));
-
         ActiveColorTheme = colorTheme;
         OnColorThemeChanged?.Invoke(ActiveColorTheme);
-        
-        
     }
 
     private void SetColorWithOpacities(string baseName, Color baseColor)
@@ -213,7 +205,10 @@ public partial class SukiTheme : Styles
         SetResource($"{baseName}50", baseColor.WithAlpha(0.5));
         SetResource($"{baseName}25", baseColor.WithAlpha(0.25));
         SetResource($"{baseName}10", baseColor.WithAlpha(0.1));
+        SetResource($"{baseName}7", baseColor.WithAlpha(0.07));
         SetResource($"{baseName}5", baseColor.WithAlpha(0.05));
+        SetResource($"{baseName}2", baseColor.WithAlpha(0.02));
+        SetResource($"{baseName}1", baseColor.WithAlpha(0.01));
     }
 
     private void SetResource(string name, Color color) =>
