46be852c00dca28b3c3329cc1e67dc178a845d17
Clement Sepulchre <sepulchreclement.dic@gmail.com>
2024-01-08 15:20:47 +0100

    small presentation changes

--- SukiUI.Demo/Features/ControlsLibrary/ButtonsLibraryView.axaml.cs
+++ SukiUI.Demo/Features/ControlsLibrary/ButtonsLibraryView.axaml.cs
@@ -1,4 +1,9 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Rendering.Composition;
+using Avalonia.Rendering.Composition.Animations;
+using Avalonia.VisualTree;
 
 namespace SukiUI.Demo.Features.ControlsLibrary;
 
@@ -7,5 +12,31 @@ public partial class ButtonsLibraryView : UserControl
     public ButtonsLibraryView()
     {
         InitializeComponent();
+
+        var w = this.Get<WrapPanel>("WrapButtons");
+            
+        w.AttachedToVisualTree += (sender, args) =>
+        {
+            CompositionVisual compositionVisual = ElementComposition.GetElementVisual(w);
+            if (compositionVisual == null)
+                return;
+        
+            Compositor compositor = compositionVisual.Compositor;
+
+            var animationGroup = compositor.CreateAnimationGroup();
+            Vector3KeyFrameAnimation offsetAnimation = compositor.CreateVector3KeyFrameAnimation();
+            offsetAnimation.Target = "Offset";
+
+            offsetAnimation.InsertExpressionKeyFrame(1.0f, "this.FinalValue");
+            offsetAnimation.Duration = TimeSpan.FromMilliseconds(300);
+
+            ImplicitAnimationCollection implicitAnimationCollection = compositor.CreateImplicitAnimationCollection();
+            animationGroup.Add(offsetAnimation);
+            implicitAnimationCollection["Offset"] = animationGroup;
+            compositionVisual.ImplicitAnimations = implicitAnimationCollection;
+        };
     }
+
+   
 }
+
