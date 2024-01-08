d4f7118180336641109a29fb3ea65bfc8d49d41e
Clement Sepulchre <sepulchreclement.dic@gmail.com>
2024-01-08 13:50:06 +0100

    same fix for lsitbox

--- SukiUI/Theme/ComboboxConverter.cs
+++ SukiUI/Theme/ComboboxConverter.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Globalization;
 using Avalonia.Controls;
 using Avalonia.Data.Converters;
+using Avalonia.Markup.Xaml;
 
 namespace SukiUI.Theme
 {
@@ -43,6 +44,34 @@ namespace SukiUI.Theme
                     if (o.ToString().Length > s.ToString().Length)
                         s = o.ToString();
                 }
+                
+                return s;
+            }
+
+            public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            {
+                throw new NotSupportedException();
+            }
+        }
+        
+        public class BiggestItemListBoxConverter : IValueConverter
+        {
+            public static readonly BiggestItemConverter Instance = new();
+
+            public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            {
+                IEnumerable? x = (IEnumerable?)value;
+                if (x is null)
+                    return "";
+
+                var s = "";
+                foreach (var o in x)
+                {
+                    if (o.ToString().Length > s.ToString().Length)
+                        s = o.ToString();
+                }
+                
+               
                 
                 return s;
             }
