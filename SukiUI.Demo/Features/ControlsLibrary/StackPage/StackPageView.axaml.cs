fe5dbdf85a0145fbbf201d9c5edf64c27bfc64ce
Aaron <aaron.j.king.95@gmail.com>
2024-01-11 21:23:40 +0000

    Remove `StackPage`, replace with shiny new `SukiStackPage`

---
+++ SukiUI.Demo/Features/ControlsLibrary/StackPage/StackPageView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SukiUI.Demo.Features.ControlsLibrary.StackPage
+{
+    public partial class StackPageView : UserControl
+    {
+        public StackPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
