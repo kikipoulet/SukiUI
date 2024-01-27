cf5bff845ccc1c9c0a57a0cd11c901c96cbd9b01
Aaron <aaron.j.king.95@gmail.com>
2024-01-27 14:46:33 +0000

    Finish work on ContextMenu
    Also renamed all the controls ported to `ControlTheme` so far.

--- SukiUI.Demo/Features/ControlsLibrary/ContextMenusViewModel.cs
+++ SukiUI.Demo/Features/ControlsLibrary/ContextMenusViewModel.cs
@@ -7,9 +7,15 @@ namespace SukiUI.Demo.Features.ControlsLibrary
     public partial class ContextMenusViewModel() : DemoPageBase("Context Menus", MaterialIconKind.Menu)
     {
         [RelayCommand]
-        public void TestClicked(bool withIcon)
+        private void OptionClicked(bool withIcon)
         {
             SukiHost.ShowToast("Clicked Context Menu", withIcon ? "You clicked the option with the icon." : "You clicked the option without the icon.");
+        }
+
+        [RelayCommand]
+        private void NestedOptionClicked()
+        {
+            SukiHost.ShowToast("Clicked Context Menu", "You clicked the nested option");
         }
     }
 }
