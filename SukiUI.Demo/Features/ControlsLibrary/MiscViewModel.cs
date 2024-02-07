5aed696dcdfe001cacb89a540d1a271330d07df4
Aaron <aaron.j.king.95@gmail.com>
2024-02-07 16:25:23 +0000

    Slightly reworked the demos of calendar and datepicker.

--- SukiUI.Demo/Features/ControlsLibrary/MiscViewModel.cs
+++ SukiUI.Demo/Features/ControlsLibrary/MiscViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -8,6 +9,8 @@ namespace SukiUI.Demo.Features.ControlsLibrary
     public partial class MiscViewModel() : DemoPageBase("Miscellaneous", MaterialIconKind.DotsHorizontalCircle)
     {
         [ObservableProperty] private bool _isBusy;
+        [ObservableProperty] private DateTime _selectedDateTime = DateTime.Today;
+        [ObservableProperty] private DateTimeOffset _selectedDateTimeOffset = DateTimeOffset.Now;
 
         [RelayCommand]
         private async Task ToggleBusy()
@@ -16,5 +19,11 @@ namespace SukiUI.Demo.Features.ControlsLibrary
             await Task.Delay(3000);
             IsBusy = false;
         }
+
+        partial void OnSelectedDateTimeChanged(DateTime value) => 
+            SelectedDateTimeOffset = value;
+
+        partial void OnSelectedDateTimeOffsetChanged(DateTimeOffset value) => 
+            SelectedDateTime = value.DateTime;
     }
 }
