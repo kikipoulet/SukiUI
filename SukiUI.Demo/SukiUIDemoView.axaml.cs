be9ff7f6732ba7760ab44031b64687012522dfbf
Aaron <aaron.j.king.95@gmail.com>
2024-02-08 18:39:13 +0000

    Tidied up the icon setting in the demo.
    Using the asset loader and a bitmap was a bit extra, just a holdover from when it was using some StreamGeometry.

--- SukiUI.Demo/SukiUIDemoView.axaml.cs
+++ SukiUI.Demo/SukiUIDemoView.axaml.cs
@@ -1,15 +1,7 @@
-using System;
-using System.Reflection;
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
-using Avalonia.Media;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
-using SukiUI.Content;
 using SukiUI.Controls;
-using SukiUI.Demo.Utilities;
 using SukiUI.Models;
 
 namespace SukiUI.Demo;
@@ -19,8 +11,6 @@ public partial class SukiUIDemoView : SukiWindow
     public SukiUIDemoView()
     {
         InitializeComponent();
-        var bitmap = new Bitmap(AssetLoader.Open(new Uri("avares://SukiUI.Demo/Assets/OIG.N5o-removebg-preview.png")));
-        Icon = new WindowIcon(bitmap);
     }
 
     private void MenuItem_OnClick(object? sender, RoutedEventArgs e)
