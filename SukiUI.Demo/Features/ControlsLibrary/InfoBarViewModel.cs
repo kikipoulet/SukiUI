ede308918393b70b8698bfb046ce0e5df39ec92d
Mccree Lee <2935876049@qq.com>
2024-05-31 11:40:53 +0800

    feat: demo page & perf

---
+++ SukiUI.Demo/Features/ControlsLibrary/InfoBarViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Material.Icons;
+
+namespace SukiUI.Demo.Features.ControlsLibrary;
+
+public partial class InfoBarViewModel() : DemoPageBase("InfoBar", MaterialIconKind.InfoOutline)
+{
+    [ObservableProperty]
+    private bool _isOpen = true;
+    
+    [ObservableProperty]
+    private bool _isClosable = true;
+    
+    [RelayCommand]
+    private void RefreshIsOpenStatus()
+    {
+        IsOpen = true;
+    }
+}
