1505202ced86a1c74c30cda604b4b79c3fb77a98
kikipoulet <sepulchreclement.dic@gmail.com>
2024-06-01 22:55:24 +0200

    calendardatepicker background

--- SukiUI/ColorTheme/NotificationColor.cs
+++ SukiUI/ColorTheme/NotificationColor.cs
@@ -7,5 +7,5 @@ public static class NotificationColor
     public static readonly SolidColorBrush InfoIconForeground = new(Color.FromRgb(47,84,235));
     public static readonly SolidColorBrush SuccessIconForeground = new(Color.FromRgb(56,158,13));
     public static readonly SolidColorBrush WarningIconForeground = new(Color.FromRgb(240,140,22));
-    public static readonly SolidColorBrush ErrorIconForeground = new(Color.FromRgb(245,34,45));
+    public static readonly SolidColorBrush ErrorIconForeground = new(Color.FromRgb(236,63,48));
 }
