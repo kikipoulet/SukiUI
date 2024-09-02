0f043ae06f1d4f2db7ad44bb99ea3150327e9fc5
kikipoulet <sepulchreclement.dic@gmail.com>
2024-09-02 11:44:30 +0000

    Deploying to gh-pages from @ kikipoulet/SukiUI@7830241bcd6afec25993fe3472c83193b526e6be 🚀

---
+++ assets/chunks/button-busy.DgCdjOX4.js
@@ -0,0 +1,1 @@
+const t="/SukiUI/controls/inputs/button-busy.gif";export{t as _};
--- assets/chunks/button-busy.Dv3v_MB8.js
+++
@@ -1,1 +0,0 @@
-const t="/SukiUI/controls/inputs/button.webp",o="/SukiUI/controls/inputs/button-busy.gif";export{t as _,o as a};
