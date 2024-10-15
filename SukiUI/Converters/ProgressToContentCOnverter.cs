fae64e5438bae870766902f9777768f9a2e1cd93
Clement Sepulchre <sepulchreclement.dic@gmail.com>
2024-10-15 14:50:28 +0200

    fixing button and busyarea loading embedding

---
+++ SukiUI/Converters/ProgressToContentCOnverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+using SukiUI.Controls;
+
+namespace SukiUI.Converters
+{
+    public class ProgressToContentCOnverter : IValueConverter
+    {
+        public static readonly ProgressToContentCOnverter Instance = new ProgressToContentCOnverter();
+        
+        
+        public object? Convert(object? value, Type targetType, object? parameter, 
+            CultureInfo culture)
+        {
+            if ((bool)value)
+                return new Loading() { LoadingStyle = LoadingStyle.Simple };
+
+            return new Panel();
+        }
+        
+        public object ConvertBack(object? value, Type targetType, 
+            object? parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
