4bf06e97a5ecfea62d54f57583ec80572d7c18dd
Peter Vietense <peter.vietense@hotmail.de>
2024-11-08 16:42:03 +0100

    fix PropertyGrid not opening complex child viewmodel in new window, added sukihost support back

--- SukiUI.Demo/Features/ControlsLibrary/PropertyGridView.axaml.cs
+++ SukiUI.Demo/Features/ControlsLibrary/PropertyGridView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using SukiUI.Controls;
 
 namespace SukiUI.Demo.Features.ControlsLibrary;
 
@@ -7,5 +10,19 @@ public partial class PropertyGridView : UserControl
     public PropertyGridView()
     {
         InitializeComponent();
+    }
+
+    private void ToggleButton_OnIsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not ToggleButton toggleButton)
+        {
+            return;
+        }
+
+        var resource = this.FindResource("PropertyGridTemplateSelector");
+        if (resource is PropertyGridTemplateSelector templateSelector)
+        {
+            templateSelector.UseSukiHost = toggleButton.IsChecked == true;
+        }
     }
 }
