44897866d7d0c6e10c0b9f04c5db48fb68448dde
dme-compunet <117437050+dme-compunet@users.noreply.github.com>
2024-11-22 10:51:37 +0200

    Adjust icon page for compile bindings

--- SukiUI.Demo/Features/ControlsLibrary/IconsViewModel.cs
+++ SukiUI.Demo/Features/ControlsLibrary/IconsViewModel.cs
@@ -1,40 +1,39 @@
 using Avalonia.Media;
 using Material.Icons;
 using SukiUI.Content;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using CommunityToolkit.Mvvm.Input;
 using SukiUI.Demo.Services;
 using SukiUI.Toasts;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace SukiUI.Demo.Features.ControlsLibrary;
 
 public partial class IconsViewModel : DemoPageBase
 {
-    public Dictionary<string, StreamGeometry> AllIcons { get; }
+    public IEnumerable<IconItemViewModel> Icons { get; }
 
-    private readonly ClipboardService _clipboardService;
-    private readonly ISukiToastManager _toastManager;
-    
     public IconsViewModel(ClipboardService clipboardService, ISukiToastManager toastManager) : base("Icons", MaterialIconKind.AlphaICircleOutline, int.MaxValue)
     {
-        _clipboardService = clipboardService;
-        _toastManager = toastManager;
-        AllIcons = typeof(Icons)
-            .GetFields(BindingFlags.Public | BindingFlags.Static)
-            .Where(x => x.FieldType == typeof(StreamGeometry))
-            .OrderBy(x => x.Name)
-            .ToDictionary(x => x.Name, y => (StreamGeometry)y.GetValue(null)!);
-    }
+        var fields = typeof(Icons).GetFields(BindingFlags.Public | BindingFlags.Static);
 
-    [RelayCommand]
-    private void OnIconClicked(string iconName)
-    {
-        _clipboardService.CopyToClipboard($"<PathIcon Data=\"{{x:Static content:Icons.{iconName}}}\" />");
-        _toastManager.CreateSimpleInfoToast()
-            .WithTitle("Copied To Clipboard")
-            .WithContent($"Copied the XAML for {iconName} to your clipboard.")
-            .Queue();
+        var icons = new List<IconItemViewModel>(fields.Length);
+
+        foreach (var field in fields)
+        {
+            if (field.GetValue(null) is not Geometry geometry)
+            {
+                continue;
+            }
+
+            var icon = new IconItemViewModel(clipboardService, toastManager)
+            {
+                Name = field.Name,
+                Geometry = geometry
+            };
+
+            icons.Add(icon);
+        }
+
+        Icons = icons;
     }
 }
