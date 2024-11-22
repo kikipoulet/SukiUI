6213d45c41b768dd44e81db282796b8efa3ae2f8
dme-compunet <117437050+dme-compunet@users.noreply.github.com>
2024-11-22 10:26:08 +0200

    Change view locator

---
+++ SukiUI.Demo/Common/SukiViews.cs
@@ -0,0 +1,81 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using SukiUI.Demo.Features;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SukiUI.Demo.Common;
+
+public class SukiViews
+{
+    private readonly Dictionary<Type, Type> _vmToViewMap = [];
+
+    public SukiViews AddView<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TView,
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TViewModel>(ServiceCollection services)
+        where TView : ContentControl
+        where TViewModel : ObservableObject
+    {
+        var viewType = typeof(TView);
+        var viewModelType = typeof(TViewModel);
+
+        _vmToViewMap.Add(viewModelType, viewType);
+
+        if (viewModelType.IsAssignableTo(typeof(DemoPageBase)))
+        {
+            services.AddSingleton(typeof(DemoPageBase), viewModelType);
+        }
+        else
+        {
+            services.AddSingleton(viewModelType);
+        }
+
+        return this;
+    }
+
+    public bool TryCreateView(IServiceProvider provider, Type viewModelType, [NotNullWhen(true)] out Control? view)
+    {
+        var viewModel = provider.GetRequiredService(viewModelType);
+
+        return TryCreateView(provider, viewModel, out view);
+    }
+
+    public bool TryCreateView(IServiceProvider provider, object? viewModel, [NotNullWhen(true)] out Control? view)
+    {
+        view = null;
+
+        if (viewModel == null)
+        {
+            return false;
+        }
+
+        var viewModelType = viewModel.GetType();
+
+        if (_vmToViewMap.TryGetValue(viewModelType, out var viewType))
+        {
+            view = Activator.CreateInstance(viewType) as Control;
+
+            if (view != null)
+            {
+                view.DataContext = viewModel;
+            }
+        }
+
+        return view != null;
+    }
+
+    public Control CreateView<TViewModel>(IServiceProvider provider) where TViewModel : ObservableObject
+    {
+        var viewModelType = typeof(TViewModel);
+
+        if (TryCreateView(provider, viewModelType, out var view))
+        {
+            return view;
+        }
+
+        throw new InvalidOperationException();
+    }
+}
