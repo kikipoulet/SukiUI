09a28130ee9dec6be8b3ea8f9c1200c6499b6455
Tiago Conceição <Tiago_caza@hotmail.com>
2025-03-21 00:29:59 +0000

    Improvements to InfoBar
    - Bind `HorizontalContentAlignment` and `VerticalContentAlignment` to allow aligns
    - Add `TitleOrientation` to be able to stack title vertically or horizontally
    - Add `MessageTextAlignment` to allow text alignment
    - Add `IconSize` to allow change icon size
    - Add `IsTextSelectable` to allow select text
    - Change grid column width to Auto instead of fixed values
    - Fix ghost icon when null (Hide in a null situation)

--- SukiUI.Demo/Features/ControlsLibrary/InfoBarViewModel.cs
+++ SukiUI.Demo/Features/ControlsLibrary/InfoBarViewModel.cs
@@ -7,14 +7,20 @@ namespace SukiUI.Demo.Features.ControlsLibrary;
 public partial class InfoBarViewModel() : DemoPageBase("InfoBar", MaterialIconKind.InfoOutline)
 {
     [ObservableProperty]
+    private int _iconSize = 12;
+
+    [ObservableProperty]
+    private bool _isTextSelectable;
+
+    [ObservableProperty]
     private bool _isOpen = true;
-    
+
     [ObservableProperty]
     private bool _isClosable = true;
-    
+
     [ObservableProperty]
     private bool _isOpaque = false;
-    
+
     [RelayCommand]
     private void RefreshIsOpenStatus()
     {
