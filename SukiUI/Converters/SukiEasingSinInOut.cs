16e8edce173705b2474f35ee8fb38fbfdefb5d08
Clement Sepulchre <sepulchreclement.dic@gmail.com>
2025-04-01 11:57:02 +0200

    #453

---
+++ SukiUI/Converters/SukiEasingSinInOut.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Animation.Easings;
+
+namespace SukiUI.Converters
+{
+    public class SukiEasingOut: Easing
+    {
+        public override double Ease(double progress)
+        {
+            progress = Math.Max(0.0, Math.Min(1.0, progress));
+            
+            const double c1 = 1.01; 
+            const double c3 = c1 + 1.0;
+            double p = progress - 1.0; 
+
+            return 1.0 + c3 * p * p * p + c1 * p * p;
+        }
+    }
+}
