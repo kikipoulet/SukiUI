423352f3cafc507827bfa2d01ac01d18153f0df9
Tiago Conceição <Tiago_caza@hotmail.com>
2025-04-12 20:17:28 +0100

    Clean Button xaml and implement colors

--- SukiUI/Enums/SukiButtonStyles.cs
+++ SukiUI/Enums/SukiButtonStyles.cs
@@ -6,19 +6,22 @@
 [Flags]
 public enum SukiButtonStyles
 {
-    Standard  = (1 << 0),
-    Basic     = (1 << 1),
-    Flat      = (1 << 2),
-    Accent    = (1 << 3),
-    Success   = (1 << 4),
-    Danger    = (1 << 5),
+    Standard     = (1 << 0),
+    Basic        = (1 << 1),
+    Flat         = (1 << 2),
+    Accent       = (1 << 3),
+    Success      = (1 << 4),
+    Information  = (1 << 5),
+    Warning      = (1 << 6),
+    Danger       = (1 << 7),
 
-    Rounded   = (1 << 6),
-    Outlined  = (1 << 7),
-    Card      = (1 << 8),
+    Rounded      = (1 << 8),
+    Outlined     = (1 << 9),
+    Card         = (1 << 10),
 
-    Icon      = (1 << 9),
-    Large     = (1 << 10),
+    Icon         = (1 << 11),
+    Large        = (1 << 12),
 
-    WindowControlsButton = (1 << 11),
+    NoPressedAnimation = (1 << 13),
+    WindowControlsButton = (1 << 14),
 }
