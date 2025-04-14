6b09bb877c424ac73d68a102e3313b2edcfcca1e
YJammak <yjk@yjk.name>
2025-04-14 11:26:17 +0800

    Modify the Checkbox indeterminate style to be similar to checked

---
+++ SukiUI/Converters/BoolConvert.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Data.Converters;
+
+namespace SukiUI.Converters;
+
+public static class BoolConvert
+{
+    public static readonly IValueConverter NotFalse =
+        new FuncValueConverter<bool?, bool>(b => b != false);
+}
