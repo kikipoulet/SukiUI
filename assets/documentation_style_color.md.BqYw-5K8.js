6c6986590d13e7daf7e7ae84a0d53e099dd6059f
kikipoulet <sepulchreclement.dic@gmail.com>
2025-07-23 07:57:31 +0000

    Deploying to gh-pages from @ kikipoulet/SukiUI@c8165421e8d5ef9b011397417ce4db623e09263d 🚀

---
+++ assets/documentation_style_color.md.BqYw-5K8.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o}from"./chunks/framework.u6_mHqbc.js";const p=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"documentation/style/color.md","filePath":"documentation/style/color.md"}'),a={name:"documentation/style/color.md"};function c(r,n,s,l,d,i){return o(),e("div")}const _=t(a,[["render",c]]);export{p as __pageData,_ as default};
