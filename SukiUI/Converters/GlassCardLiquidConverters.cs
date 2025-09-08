5df4ad9da4f9c05702b14bc546ef7caa8cad7e69
Clement Sepulchre <sepulchreclement.dic@gmail.com>
2025-09-08 11:51:28 +0200

    subtle 'liquid' glass option

---
+++ SukiUI/Converters/GlassCardLiquidConverters.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+
+namespace SukiUI.Converters
+{
+    public sealed class StartPointLiquidConverter : IMultiValueConverter
+    {
+
+        public static StartPointLiquidConverter Instance = new StartPointLiquidConverter();
+        
+        public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            
+            if (values[0] is double w && values[1] is double h)
+            {
+                var min = Math.Min(h,w);
+                var max = Math.Max(h,w);
+                var factor = Math.Abs( min/max);
+
+                var y = -( (1/ ( 1.75* factor)) )  ;
+
+                return new RelativePoint(0.2, y , RelativeUnit.Relative);
+            }
+
+            return AvaloniaProperty.UnsetValue;
+        }
+    }
+    
+    public sealed class EndPointLiquidConverter : IMultiValueConverter
+    {
+
+        public static EndPointLiquidConverter Instance = new EndPointLiquidConverter();
+        
+        public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            
+            if (values[0] is double w && values[1] is double h)
+            {
+                var min = Math.Min(h,w);
+                var max = Math.Max(h,w);
+                var factor = Math.Abs( min/max);
+
+                var y = ( (1/ ( 1.75* factor)) )  ;
+
+                return new RelativePoint(0.8, 1+ y , RelativeUnit.Relative);
+            }
+
+            return AvaloniaProperty.UnsetValue;
+        }
+    }
+}
